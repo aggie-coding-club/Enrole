8d05abe52408a56f807775384f104b792a8b7e2e
Jesse Phipps <jessefphipps@gmail.com>
2021-03-11 13:58:07 -0600

    Changed directory in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sot_alpha/main.dart';
+import 'package:enrole_app_dev/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
